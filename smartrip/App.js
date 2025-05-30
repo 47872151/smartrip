33250f7f3c71e1e1880d7fbe7f86a2a76af7aa04
Android Studio <devandroid@ort>
2025-05-30 09:54:20 -0300

    iniciar sesion

--- smartrip/App.js
+++ smartrip/App.js
@@ -1,14 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 import * as SplashScreen from 'expo-splash-screen';
-SplashScreen.preventAutoHideAsync();
+import { ImageBackground } from 'react-native';
+
 export default function App() {
-  SplashScreen.hideAsync();
+  const [user, setUser] = React.useState("");
+  const [contraseña, setContraseña] = React.useState("");
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        setTimeout(async () => {
+          await SplashScreen.hideAsync();
+        }, 1000);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <View style={styles.container}>  
+        <StatusBar style="auto" /> 
+        <View style={styles.form}>   
+        <Text style={ styles.texto }>¿Cuál es tú número de teléfono o email?</Text>
+        <TextInput style={styles.input} placeholder="Introducí tú número de teléfono o email" placeholderTextColor="rgba(3, 4, 94, 0.6)" value={user} onChangeText={setUser} />
+        <TextInput secureTextEntry={true} placeholder="Contraseña" placeholderTextColor="rgba(3, 4, 94, 0.6)" value={contraseña} onChangeText={setContraseña} style={styles.input} />
+        <View style={{ width: '100%' }}>
+        <Text style={styles.olvPass}>Olvidé mi contraseña</Text>
+        </View>
+        <View style={styles.botones}>
+        </View>
+        <Pressable style={styles.button1}>
+          <Text style={styles.buttonText1}>Iniciar sesión</Text>    
+          <FontAwesome name="sign-in" size={20} color="white" marginLeft="10" />
+        </Pressable>
+        <Pressable style={styles.button2}>
+          <FontAwesome name="google" size={20} color="#03045E" marginRight="5" />
+          <Text style={styles.buttonText2}>Continuar con Google</Text>
+        </Pressable>
+        <Pressable style={styles.button1}>
+          <Text style={styles.buttonText1}>Registrarse</Text>
+        </Pressable>
+        </View>
+        <View style={styles.logoContainer}>
+          <ImageBackground source={require('./assets/LOGO.png')} resizeMode='contain' style={styles.image} />
+        </View>
+
+      </View>
   );
 }
 
@@ -18,5 +61,80 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  form:{
+    width: "100%",
+    marginTop: 100,
+    marginBottom: 100,
+  },
+  image: {
+    width: 200,
+    height: 100,
+    marginTop: 20,
+  },  
+  logoContainer: {
+    marginBottom: -30, // Empuja hacia abajo
+    },  
+  olvPass: {
+    textAlign: "right",
+    fontSize: 16,
+    marginBottom: 5,
+    color: "#03045E",
+    textDecorationLine: 'underline',
+  },
+  botones:{
+    width: '100%',
+    borderTopWidth: 1,
+    margin: 20,
+    marginLeft: 3,
+    borderColor: '#03045E',
+  },
+  input: {
+    width: '100%',
+    borderWidth: 5,
+    borderColor: '#CAF0F8',
+    borderRadius: 20,
+    padding: 12,
+    marginVertical: 8,
+    fontSize: 18,
+    backgroundColor: '#CAF0F8',
+    color: '#03045E',
+  },  
+  texto: {
+    fontSize: 26,
+    color: "#03045E",
+  },
+  button1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 18,
+    marginVertical: 5,
+    backgroundColor: '#00B4D8',
+    borderRadius: 15,
+    width: '100%',
+    justifyContent: 'center',
+  },
+  button2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 18,
+    marginVertical: 5,
+    backgroundColor: '#CAF0F8',
+    borderRadius: 15,
+    width: '100%',
+    justifyContent: 'center',
+  },
+  buttonText1: {
+    marginLeft: 8,
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 18,
+  },
+  buttonText2: {
+    marginLeft: 8,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#03045E'
   },
 });
